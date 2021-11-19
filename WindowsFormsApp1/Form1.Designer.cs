7f0f841dd092cebb41d08926398f7758f068a51d
xiaojunyong <1149733056@qq.com>
2021-11-19 20:46:48 +0800

    lan

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -57,13 +57,13 @@
             this.label18 = new System.Windows.Forms.Label();
             this.label19 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label22 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.label21 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.label20 = new System.Windows.Forms.Label();
-            this.label22 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
@@ -82,15 +82,15 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 351);
+            this.label1.Location = new System.Drawing.Point(7, 354);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(83, 17);
+            this.label1.Size = new System.Drawing.Size(113, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "WIFI account";
+            this.label1.Text = "WIFI account 2.4G";
             // 
             // wifiname
             // 
-            this.wifiname.Location = new System.Drawing.Point(113, 348);
+            this.wifiname.Location = new System.Drawing.Point(132, 351);
             this.wifiname.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.wifiname.Name = "wifiname";
             this.wifiname.Size = new System.Drawing.Size(406, 23);
@@ -99,15 +99,15 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 390);
+            this.label2.Location = new System.Drawing.Point(7, 393);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(95, 17);
+            this.label2.Size = new System.Drawing.Size(125, 17);
             this.label2.TabIndex = 3;
-            this.label2.Text = "WIFI password";
+            this.label2.Text = "WIFI password 2.4G";
             // 
             // wifipwd
             // 
-            this.wifipwd.Location = new System.Drawing.Point(113, 387);
+            this.wifipwd.Location = new System.Drawing.Point(132, 387);
             this.wifipwd.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.wifipwd.Name = "wifipwd";
             this.wifipwd.Size = new System.Drawing.Size(406, 23);
@@ -281,9 +281,9 @@
             this.label12.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.label12.Location = new System.Drawing.Point(49, 228);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(371, 17);
+            this.label12.Size = new System.Drawing.Size(375, 17);
             this.label12.TabIndex = 21;
-            this.label12.Text = "Website style is \"https://remoteplay.dl.playstation.net/ * * * * *\"";
+            this.label12.Text = "Website style is \"https://remoteplay.dl.playstation.net/ * * * * *\" ";
             // 
             // label13
             // 
@@ -306,7 +306,7 @@
             this.label14.Size = new System.Drawing.Size(551, 35);
             this.label14.TabIndex = 23;
             this.label14.Text = "Input the same PSN account and password you used to log into PS5, After loginning" +
-    " successfully, copy and paste the \"Full URL\" in the bottom.";
+    " successfully, copy and paste the \"Full URL\" in the bottom. CTRL+V";
             // 
             // label15
             // 
@@ -413,6 +413,17 @@
             this.groupBox1.Text = "config";
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // label22
+            // 
+            this.label22.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label22.ForeColor = System.Drawing.Color.Red;
+            this.label22.Location = new System.Drawing.Point(47, 433);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(590, 18);
+            this.label22.TabIndex = 29;
+            this.label22.Text = "Choose from the ps5 system (Settings > System> Remote Play (Enable Remote Play) >" +
+    " Link Device)";
+            // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.label21);
@@ -476,17 +487,6 @@
             this.label20.Size = new System.Drawing.Size(61, 17);
             this.label20.TabIndex = 0;
             this.label20.Text = "升级/还原";
-            // 
-            // label22
-            // 
-            this.label22.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label22.ForeColor = System.Drawing.Color.Red;
-            this.label22.Location = new System.Drawing.Point(47, 433);
-            this.label22.Name = "label22";
-            this.label22.Size = new System.Drawing.Size(590, 18);
-            this.label22.TabIndex = 29;
-            this.label22.Text = "Choose from the ps5 system (Settings > System> Remote Play (Enable Remote Play) >" +
-    " Link Device)";
             // 
             // Form1
             // 
